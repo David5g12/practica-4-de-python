1e96df4b1716ef08ff3107cc4f23ec20cac21e18
David5g12 <145526876+David5g12@users.noreply.github.com>
2023-10-02 21:28:51 -0600

    Agregar archivos de proyecto.

---
+++ practica 4 de python/practica_4_de_python.py
@@ -0,0 +1,15 @@
+maxf = 3
+maxc = 5
+a = [[0.0] * maxc for _ in range(maxf)]  
+# Leer el array
+print("Asigna los numeros para rellenar la matriz de 3x5:")
+for f in range(maxf):
+    for c in range(maxc):
+        a[f][c] = float(input())
+
+# Escribir el array
+print("La matriz resultante de 3x5 es:")
+for f in range(maxf):
+    for c in range(maxc):
+        print(a[f][c], end=" ") 
+    print() 
